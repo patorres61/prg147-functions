5844002ea46d4ac696aff4dca3902d68dc7bf30a
Phyllis Torres <ptorres@students.mchenry.edu>
2016-08-30 21:28:22 -0500

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,60 @@
+function change01() {
+    'use strict';
+    document.getElementById("large").src = "images/chimps.jpg";
+    document.getElementById("greeting").innerHTML = 'So, Ralphie, when are Moe and Curly gonna show so we can do the "See No Evil" act?';
+
+}
+
+function change02() {
+    'use strict';
+    document.getElementById("large").src = "images/littlepiggies.jpg";
+    document.getElementById("greeting").innerHTML = 'So, what is this food called bacon that everyone is talking about? I hear it goes with everything!';
+}
+
+function change03() {
+    'use strict';
+    document.getElementById("large").src = "images/threetoads.jpg";
+    document.getElementById('greeting').innerHTML = "Don't make me say it! You know you're thinking it, too! Do not say I didn't warn you! Yup, Menage a frog!!!";
+}
+
+function change04() {
+    'use strict';
+    document.getElementById("large").src = "images/catvampire.jpg";
+    document.getElementById('greeting').innerHTML = 'Nailed it! I totally look like a vampire!';
+}
+
+function change05() {
+    'use strict';
+    document.getElementById("large").src = "images/botswanakhudu.jpg";
+    document.getElementById('greeting').innerHTML = 'Has anyone seen my straightener?';
+}
+
+function change06() {
+    'use strict';
+    document.getElementById("large").src = "images/ifpigscouldfly.jpg";
+    document.getElementById('greeting').innerHTML = 'This is great and all for taking a relaxing cruise around the bay. Now, if only pigs could fly!';
+}
+
+function change07() {
+    'use strict';
+    document.getElementById("large").src = "images/elephants.jpg";
+    document.getElementById('greeting').innerHTML = 'Wait, you go to your left, and I will move to my right!';
+}
+
+function change08() {
+    'use strict';
+    document.getElementById("large").src = "images/bittern.jpg";
+    document.getElementById('greeting').innerHTML = "Everbody's gone surfin...Surfin' USA! Man, this surfing thing is a piece of cake!";
+}
+
+function change09() {
+    'use strict';
+    document.getElementById("large").src = "images/bugs.jpg";
+    document.getElementById('greeting').innerHTML = 'Get your head out of the toilet! I am practicing the Heimlich Maneuver!';
+}
+
+function change10() {
+    'use strict';
+    document.getElementById("large").src = "images/horses.jpg";
+    document.getElementById('greeting').innerHTML = "Let's give them a dose of...Crazy Eyes!";
+}
